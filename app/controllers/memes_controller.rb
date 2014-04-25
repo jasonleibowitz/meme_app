036696cc4c716b91d70130ed4130d7415a415224
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-25 10:50:58 -0400

    added create to controller

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,7 +1,12 @@
 class MemeController < ApplicationController
 
-def create
-  @meme = Meme.new
-end
+  def create
+    @meme = Meme.create(meme_params)
+  end
+
+  private
+    def meme_params
+      params.require(:meme).permit(:name, :hipster_level, :outdatedness, :glasses_worn)
+    end
 
 end
