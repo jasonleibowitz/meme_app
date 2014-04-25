2734beda71221b630dadb1d9247ca051189a80b6
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-25 10:27:13 -0400

    initial init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MemeApp::Application.config.secret_key_base = '0c75f37f473342c187008f43a425b10ffda0ed7062f80fb551f4e7e88c16fda249669900090165b969074291e7de19d94bc657244f19174e9fb4cf9b63aa4e3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MemeApp::Application.config.session_store :cookie_store, key: '_meme_app_session'
